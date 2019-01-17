2d064260288d43c4ab38e895319e16fb898996f1
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-01-17 15:48:06 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,55 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <Post />
+      </div>
+    );
+  }
+}
+
+const headerHidden = <h1 className="header">Informational Affairs</h1>;
+const headerShown = (
+  <h1 className="header">
+    Informational Affairs is an ever growing index of books collected by <a href="#">Folder Studio</a>. I'm copying the whole thing for practice.
+  </h1>
+);
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {isVisible: false}
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        isVisible: !prevState.isVisible
+      };
+    });
+  }
+  render() {
+    return (
+      <header>
+        <div>
+          {this.state.isVisible===false ? headerHidden : headerShown}
+        </div>
+        <div>
+          <h1 className="open" onClick={this.handleToggleVisibility}>
+            {this.state.isVisible===false ? '( ? )' : '( x )'}
+          </h1>
+        </div>
+      </header>
+    );
+  }
+}
+
+class Post extends Component {
+  render() {
+    return (
+      <div className="post">
+        <img src="/src/photos/01.jpg" />
       </div>
     );
   }
