e99b3d95ee164cc0967ba1eb5200e0fd418c9083
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-03-05 10:44:23 -0800

    created file for archive array and imported it into Posts.js.

---
+++ src/archive.js
@@ -0,0 +1,120 @@
+import Images from "./images/index";
+
+const archive = [
+  {
+    images: [
+      Images.img253_01,
+      Images.img253_02,
+      Images.img253_03,
+      Images.img253_04,
+      Images.img253_05
+    ],
+    id: 253,
+    year: 1982,
+    title: "ぎんざ1982",
+    publisher: "東京: 日本リクルートセンター",
+    tags: [
+      "book",
+      "1982",
+      "japan",
+      "tokyo",
+      "hardcover",
+      "spreads",
+      "cover",
+      "photography",
+      "ginza"
+    ]
+  },
+  {
+    images: [
+      Images.img252_01,
+      Images.img252_02,
+      Images.img252_03,
+      Images.img252_04
+    ],
+    id: 252,
+    year: 2002,
+    author: "Joshua Tree National Park Association.",
+    title: "Wildflowers of Joshua Tree National Park",
+    publisher: "California: JTNPA",
+    tags: [
+      "brochure",
+      "2002",
+      "wildflowers",
+      "joshua tree",
+      "national park",
+      "mojave",
+      "desert",
+      "spreads",
+      "typography",
+      "photography"
+    ]
+  },
+  {
+    images: [
+      Images.img250_01,
+      Images.img250_02,
+      Images.img250_03,
+      Images.img250_04
+    ],
+    id: 250,
+    year: 1982,
+    author: "Riehl, Rüdiger.",
+    title: "Aquarium Atlas",
+    publisher: "West Germany: Hans A. Baensch",
+    tags: [
+      "book",
+      "1982",
+      "fish",
+      "aquarium",
+      "hardcover",
+      "cover",
+      "spreads",
+      "photography"
+    ]
+  },
+  {
+    images: [
+      Images.img249_01,
+      Images.img249_02,
+      Images.img249_03,
+      Images.img249_04,
+      Images.img249_05,
+      Images.img249_06
+    ],
+    id: 249,
+    year: 1984,
+    author: "Chatani, Masahiro.",
+    title: "Pattern Sheets of Origamic Architecture",
+    publisher: "Tokyo: Shokokusha",
+    tags: [
+      "book",
+      "1984",
+      "origami",
+      "architecture",
+      "cover",
+      "spreads",
+      "linework"
+    ]
+  },
+  {
+    images: [Images.img247_01],
+    id: 247,
+    year: 1984,
+    author: "ノンノ・モア・ブックス.",
+    title: "NON・NO クッキング・ブック PART 2: お弁当とおそうざいの本",
+    publisher: "東京: 集英社",
+    tags: [
+      "magazine",
+      "1984",
+      "cooking",
+      "bento",
+      "softcover",
+      "cover",
+      "spreads",
+      "photography"
+    ]
+  }
+];
+
+export default archive;
